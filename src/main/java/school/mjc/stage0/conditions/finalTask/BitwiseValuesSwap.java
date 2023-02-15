bcdafabc77a7978895bed3942611ccda4097f13d
Vladislav Samorukov <smrk.vld@gmail.com>
2023-02-15 21:22:56 +0300

    done2

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -2,8 +2,9 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class BitwiseValuesSwap {
     public void swap(int first, int second) {
-        first ^= second;
-        second ^= first;
-        first ^= second;
+        first = first ^ second;
+        second = second ^ first;
+        first = first ^ second;
+        System.out.println(first + " " + second);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -8,8 +8,8 @@ public class DaysInMonth {
         }
 
         System.out.println(switch (month) {
-            case 1, 3, 5, 6, 8, 10, 12 -> 31;
-            case 4, 7, 9, 11 -> 30;
+            case 1, 3, 5, 7, 8, 10, 12 -> 31;
+            case 4, 6, 9, 11 -> 30;
             case 2 -> year % 4 == 0 && (year % 100 != 0 || year % 400 == 0) ? 29 : 28;
             default -> "invalid date";
         });
