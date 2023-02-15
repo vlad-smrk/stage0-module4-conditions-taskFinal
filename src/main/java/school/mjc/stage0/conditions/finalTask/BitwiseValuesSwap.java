26a8407c9fee593eeef2c3fa0bf78cf2f4e011fb
Vladislav Samorukov <smrk.vld@gmail.com>
2023-02-15 21:27:57 +0300

    done3

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -5,6 +5,7 @@ public class BitwiseValuesSwap {
         first = first ^ second;
         second = second ^ first;
         first = first ^ second;
-        System.out.println(first + " " + second);
+        System.out.println(first);
+        System.out.println(second);;
     }
 }
